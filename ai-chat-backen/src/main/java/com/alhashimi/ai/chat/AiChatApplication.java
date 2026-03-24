b42b4a3d2dec46edd929f726d60ce27d5c6f7b85
Moujahed Khouja <mk@socialcard.de>
2026-03-24 20:23:59 +0100

    ABC-123: change main method to static in AiChatApplication

--- ai-chat-backen/src/main/java/com/alhashimi/ai/chat/AiChatApplication.java
+++ ai-chat-backen/src/main/java/com/alhashimi/ai/chat/AiChatApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AiChatApplication {
 
-	public static void main(String[] args) {
+	 static void main(String[] args) {
 		SpringApplication.run(AiChatApplication.class, args);
 	}
 
